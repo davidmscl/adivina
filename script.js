de7583cdc53b131160a3281910dd6f9c94660c19
davidmscl <davidm.scl@gmail.com>
2024-09-19 00:10:51 -0300

    ios change

--- script.js
+++ script.js
@@ -118,21 +118,22 @@ function iniciarTemporizador() {
 }
 
 function requestDeviceOrientation ()   {
-    // feature detect
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('deviceorientation', function(event) {
-                detectarMovimiento(event);
-                });
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-      window.addEventListener('deviceorientation', detectarMovimiento, false);
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        // Handle iOS 13+ devices.
+        DeviceMotionEvent.requestPermission()
+          .then((state) => {
+            if (state === 'granted') {
+                window.addEventListener('deviceorientation', detectarMovimiento);
+            } else {
+              console.error('Request to access the orientation was rejected');
+            }
+          })
+          .catch(console.error);
+      } else {
+        // Handle regular non iOS 13+ devices.
+        window.addEventListener('deviceorientation', detectarMovimiento, false);
     }
+    
   }
 
 
